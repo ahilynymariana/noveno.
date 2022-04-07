42a5f86319e62b639f8640eaf879a12a60e4e826
ahilynymariana <ahilyn.brown@cea.edu.co>
2022-04-07 14:31:06 -0500

    xdhtndetj

---
+++ SRC/practica.js
@@ -0,0 +1,23 @@
+/* JavaScript como todo lenguaje de programacion se basa en la utilizacion de palabras reservadas */
+// Creacion de Variables 
+/*var nom = "Diego" ; // nose recomienda usarla
+const nombre = "sofia" ;
+let apellido = "sanchez" ;
+
+alert (nombre);
+*/
+
+let numero = 3;
+let numero1 = 2;
+let suma = numero + numero1;
+alert (suma);
+
+/* operadores aritematicos en JavaScript
+     * aficion
+     - resta
+     * multiplicacion
+     / divicion
+     % residuo
+     /*
+
+    
