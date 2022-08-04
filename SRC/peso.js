54ddc9b84602d8a357a223c9d01bec606222fb41
ahilynymariana <99908914+ahilynymariana@users.noreply.github.com>
2022-08-04 08:47:09 -0500

    peso

--- SRC/peso.js
+++ SRC/peso.js
@@ -22,5 +22,19 @@ let nombre;
 //Inicializados
 nombre = "Anastasia";
 //Para inicializar una variable se usa el signo "=" (llamado operador de asignacion)
-// Pere en Javascript puedo realizar ambas cosas (Declaracion e inicializacion) en una sola linea
+// Per0 en Javascript puedo realizar ambas cosas (Declaracion e inicializacion) en una sola linea
 let numero = 45;
+//Javascript como todo lenguaje de programacion utiliza palabras reservadas. Por ejemplo, para crear variables se puedenusar 3 de estas palabras: var (no es recomendada en el plano actual), Let y const.
+/*Tipos de Datos 
+En Javascript exiten varios tipos de datos:
+Primitivos 
+Number
+String
+Boolean
+
+Array
+Object
+*/
+//Javascript es un lenguaje con un tipado debil y dinamico junto lo que significa que una variante puede tomar un tipo de dato y luego cambiar a otro sin necesidad de ser estricto en su creacion o en su tipo de dato.
+//Array
+//
