51e824bb117a8ecf0fffe03a4b3935e39560a2b4
ahilynymariana <99908914+ahilynymariana@users.noreply.github.com>
2022-08-18 08:47:17 -0500

    peso

--- SRC/peso.js
+++ SRC/peso.js
@@ -37,4 +37,33 @@ Object
 */
 //Javascript es un lenguaje con un tipado debil y dinamico junto lo que significa que una variante puede tomar un tipo de dato y luego cambiar a otro sin necesidad de ser estricto en su creacion o en su tipo de dato.
 //Array
-//
+es un tipo de estrutura de datos que me permite organizar valores en una sola variable. puede contener diferentes tipos de datos , tales como numeros, cadenas, objectos y otros arrays 
+
+let nombre =[];
+nombres = ["juan", "felipe", "franco", "omar"];
+
+un array organiza sus datos con indices, los cuales indican la posicion con numeros iniciando con el numero 0. esta caracteristica me permite acceder al contenido del array en un lugar especifico que pueda necesitar 
+
+console.log(nombres [1]); //la sengunda posición del array ("felipe") */
+
+//funciones 
+let a = 3;
+let b= 8;
+let numero2 = a+b;
+//palabra reservada: es una palabra que le indica a un lenguaje de programación que ejecute una instrución o que aparte un espacio de memoria. algunas de ellas no se pueden usar como nombres variables
+funcion sumar () {
+    console.log(numero2);
+}
+sumar();
+//las funciones son instrociones determinadas para ejecutarse en el momento que son invocadas o llamadas. Son útiles porque permiten reutilizar código las veces que se necesario
+//hay dos tipos de funciones: las declarativas y las de expresión
+
+let suma = function(){
+    cosole.log(numero2);
+}
+function sumar (variable1, variable2){
+    let variable = variable1+variable2;
+    console.log(variable);
+}
+sumar(a, b);
+//al momento de utilizar una funcion debnemos masegurarnos de que las variables que usemos como parametros estén previamente definidas
