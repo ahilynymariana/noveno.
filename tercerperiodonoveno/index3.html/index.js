a9dacba5efbf54236fb3ec0292e750da8d538218
ahilynymariana <ahilyn.brown@cea.edu.co>
2022-10-20 08:58:25 -0500

    hh

--- tercerperiodonoveno/index3.html/index.js
+++ tercerperiodonoveno/index3.html/index.js
@@ -2,5 +2,9 @@ window.onload = ( ) => {
     const formulario = documen.getelementByid("formulario-pendientes");
     formulario.onsubnit = (e) => {
         e.preventDefault();
+        const pendiente = document.getElementById(pendiente);
+        const contenidopendiente = pendiente.Value;
+        pendient.value =;
+        console.log (contenidopendipente);
     }
 }
